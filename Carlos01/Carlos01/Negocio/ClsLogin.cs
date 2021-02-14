a05bb961af204d99c16cd92cdbb8df682786ded7
carlosjavierjimenezvasquez <69540480+carlosjavierjimenezvasquez@users.noreply.github.com>
2021-02-13 21:11:24 -0600

    Clase01

---
+++ Carlos01/Carlos01/Negocio/ClsLogin.cs
@@ -0,0 +1,29 @@
+﻿using Carlos01.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carlos01.Negocio
+{
+    class ClsLogin
+    {
+        Login log = new Login();
+
+            public int acceso(Login log)
+            {
+
+            int estado = 0;
+            if (log.Usuario.Equals("Carlos") && log.Password.Equals("1234")) 
+                estado = 1;
+            {
+
+
+            }
+
+            return estado;
+        
+        }
+    }
+}
